409b1a1048caa9135cf26a2cf7506d13f64be3aa
Douglas Cerna <douglascerna@yahoo.com>
2020-02-04 09:24:06 -0600

    Create dmdSec for non-ascii filenames
    This handles non-ascii filenames in a metadata/metadata.csv import.
    
    Two other fixes were required first:
    
    - Currently, if a transfer contains files with non-ascii names, the
      MCPServer will crash without starting the transfer.
    
    - Then the helper that creates MCPClient jobs needed to be updated to
      accept the decoded non-ascii arguments.
    
    Cherry-picked from https://github.com/artefactual/archivematica/commit/7ac6501fde87eae13e18dc877962a8bf3a4fb347

--- src/MCPServer/tests/test_package.py
+++ src/MCPServer/tests/test_package.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import uuid
@@ -121,6 +123,159 @@ def test_transfer_get_or_create_from_db_path_with_uuid(tmp_path):
         pytest.fail(
             "Transfer.get_or_create_from_db_by_path didn't create a Transfer model"
         )
+
+
+@pytest.mark.django_db(transaction=True)
+def test_reload_file_list(tmp_path):
+
+    # Create a transfer that will be updated through time to simulate
+    # Archivematica's processing.
+    transfer_uuid = uuid.uuid4()
+    transfer_path = tmp_path / "test-transfer-{}".format(transfer_uuid)
+    transfer = Transfer.get_or_create_from_db_by_path(str(transfer_path))
+
+    # Add files to the transfer to simulate a transfer existing on disk.
+    transfer_path.mkdir()
+    objects_path = transfer_path / "objects"
+    objects_path.mkdir()
+    first_file = objects_path / "file.txt"
+    first_file.touch()
+
+    kwargs = {
+        "uuid": uuid.uuid4(),
+        "currentlocation": "".join(
+            [transfer.REPLACEMENT_PATH_STRING, str(Path("/objects/file.txt"))]
+        ),
+        "filegrpuse": "original",
+        "transfer_id": transfer_uuid,
+    }
+    models.File.objects.create(**kwargs)
+    assert models.File.objects.filter(transfer_id=str(transfer_uuid)).count() == 1
+
+    # Add a new file to the file-system, e.g. to simulate normalization for
+    # preservation adding a new object.
+    new_file = objects_path / "new_file.txt"
+    new_file.touch()
+
+    # One file will be returned from the database  with a UUID, another from
+    # the filesystem without a UUID.
+    for file_count, file_info in enumerate(transfer.files(None, None, "/objects"), 1):
+        assert "%fileUUID%" in file_info
+        assert "%fileGrpUse%" in file_info
+        assert "%relativeLocation%" in file_info
+        if file_info.get("%fileUUID%") != "None":
+            continue
+        assert file_info.get("%relativeLocation%") == str(new_file)
+        file_path = "".join(
+            [
+                transfer.REPLACEMENT_PATH_STRING,
+                "/objects",
+                file_info.get("%relativeLocation%").split("objects")[1],
+            ]
+        )
+        kwargs = {
+            "uuid": uuid.uuid4(),
+            "currentlocation": file_path,
+            "filegrpuse": "original",
+            "transfer_id": transfer_uuid,
+        }
+        models.File.objects.create(**kwargs)
+    assert (
+        file_count == 2
+    ), "Database and file objects were not returned by the generator"
+    assert models.File.objects.filter(transfer_id=str(transfer_uuid)).count() == 2
+
+    # Simulate an additional file object being added later on in the transfer
+    # in a sub directory of the objects folder, e.g. transcribe contents.
+    sub_dir = objects_path / "subdir"
+    sub_dir.mkdir()
+    new_file = sub_dir / "another_new_file.txt"
+    new_file.touch()
+    for file_count, file_info in enumerate(transfer.files(None, None, "/objects"), 1):
+        file_path = "".join(
+            [
+                transfer.REPLACEMENT_PATH_STRING,
+                "/objects",
+                file_info.get("%relativeLocation%").split("objects")[1],
+            ]
+        )
+        if file_info.get("%fileUUID%") != "None":
+            continue
+        kwargs = {
+            "uuid": uuid.uuid4(),
+            "currentlocation": file_path,
+            "filegrpuse": "original",
+            "transfer_id": transfer_uuid,
+        }
+        models.File.objects.create(**kwargs)
+    assert (
+        file_count == 3
+    ), "Database and file objects were not returned by the generator"
+    assert models.File.objects.filter(transfer_id=str(transfer_uuid)).count() == 3
+
+    # Now the database is updated, we will still have the same file count, but
+    # all objects will be returned from the database and we will have uuids.
+    for file_count, file_info in enumerate(transfer.files(None, None, "/objects"), 1):
+        if file_info.get("%fileUUID%") == "None":
+            assert (
+                False
+            ), "Non-database entries returned from package.files(): {}".format(
+                file_info
+            )
+    assert file_count == 3
+
+    # Finally, let's just see if the scan works for a slightly larger no.
+    # files, i.e. a number with an increment slightly larger than one.
+    files = ["f1", "f2", "f3", "f4", "f5"]
+    for file_ in files:
+        new_file = objects_path / file_
+        new_file.touch()
+    new_count = 0
+    for file_count, file_info in enumerate(transfer.files(None, None, "/objects"), 1):
+        if file_info.get("%fileUUID%") == "None":
+            new_count += 1
+    assert new_count == 5
+    assert file_count == 8
+
+    # Clean up state and ensure test doesn't interfere with other transfers
+    # expected to be in the database, e.g. in test_queues.py.
+    models.File.objects.filter(transfer_id=str(transfer_uuid)).delete()
+
+
+@pytest.mark.django_db(transaction=True)
+def test_package_files_with_non_ascii_names(tmp_path):
+
+    # Create a Transfer package
+    transfer_uuid = uuid.uuid4()
+    transfer_path = tmp_path / "test-transfer-{}".format(transfer_uuid)
+    transfer = Transfer.get_or_create_from_db_by_path(str(transfer_path))
+
+    # Add a file to the transfer with non-ascii name
+    transfer_path.mkdir()
+    objects = transfer_path / "objects"
+    objects.mkdir()
+    file_ = objects / "montréal.txt"
+    file_.touch()
+
+    # Create a File model instance for the transfer file
+    kwargs = {
+        "uuid": uuid.uuid4(),
+        "currentlocation": "".join(
+            [transfer.REPLACEMENT_PATH_STRING, "/objects/montréal.txt"]
+        ),
+        "filegrpuse": "original",
+        "transfer_id": transfer_uuid,
+    }
+    models.File.objects.create(**kwargs)
+
+    # Assert only one file is returned
+    result = list(transfer.files(None, None, "/objects"))
+    assert len(result) == 1
+
+    # And it is the file we just created
+    assert result[0]["%fileUUID%"] == str(kwargs["uuid"])
+    assert result[0]["%currentLocation%"] == kwargs["currentlocation"]
+    assert result[0]["%fileGrpUse%"] == kwargs["filegrpuse"]
 
 
 class TestPadDestinationFilePath:
